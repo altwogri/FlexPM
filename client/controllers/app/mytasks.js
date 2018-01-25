1dc746ffd638736ed12b5bce52e41931e561968b
Wolfram Ogris <wolfram.ogris@flex.com>
2018-01-25 05:54:00 -0800

    [Studio] Modified screen "MyTasks"

--- client/controllers/app/mytasks.js
+++ client/controllers/app/mytasks.js
@@ -5,17 +5,17 @@ function app_mytasks($scope, app) {
 }
 
 //function approve_task(page,params) {
-$scope.approveTask = function () {
-    var xmlhttp = new XMLHttpRequest();
-    var url = "https://flexpm-it.flextronics.com:8015/service.asmx?op=PerformWFAction";
-    var reqResponse = "";
-    xmlhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            var response = JSON.parse(this.responseText);
-        }
-    };
-    xmlhttp.open("POST", url, false);
-    xmlhttp.send("UserAD=data.username&Group='Infor ERP'&TaskNo=item.task&Action='PDA done'");
+    $scope.approveTask = function () {
+        var xmlhttp = new XMLHttpRequest();
+        var url = "https://flexpm-it.flextronics.com:8015/service.asmx?op=PerformWFAction";
+        var reqResponse = "";
+        xmlhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                var response = JSON.parse(this.responseText);
+            }
+        };
+        xmlhttp.open("POST", url, false);
+        xmlhttp.send("UserAD=data.username&Group='Infor%20ERP'&TaskNo=item.task&Action='PDA%20done'");
 
     //Is the below method is used to pass the response to the next page?
     // page.data(function(data) {
